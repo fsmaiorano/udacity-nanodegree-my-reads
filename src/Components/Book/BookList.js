1e31b93e0a1045944bd3b8b33f0d99e462b81ab2
Fábio Maiorano <fsmaiorano@gmail.com>
2017-12-10 15:30:11 -0200

    Update: List of books with categorys

--- src/Components/Book/BookList.js
+++ src/Components/Book/BookList.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import BookShelf from './BookShelf';
 
 class BookList extends Component {
-
-    render(){
-        return(
-            "list"
+    render() {
+        const { books, onChange } = this.props;
+        return (
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <div>
+                        <BookShelf books={books.filter((book) => (book.shelf === "currentlyReading"))} title="Currently Reading" updateBookList={onChange} />
+                        <BookShelf books={books.filter((book) => (book.shelf === "wantToRead"))} title="Want to read" updateBookList={onChange} />
+                        <BookShelf books={books.filter((book) => (book.shelf === "read"))} title="Read" updateBookList={onChange} />
+                    </div>
+                </div>
+                <div className="open-search">
+                    <Link to='/search'>Add a book</Link>
+                </div>
+            </div>
         )
     }
 }
