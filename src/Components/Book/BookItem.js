7023394ae8281f60d1651c6b107c8cafe1307f3e
Fábio Maiorano <fsmaiorano@gmail.com>
2017-12-10 15:31:05 -0200

    Add: Book in LIST format

---
+++ src/Components/Book/BookItem.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+class BookItem extends Component {
+
+    changeShelf = (e) => {
+        this.props.updateBookShelf(e.target.value)
+    }
+
+    render() {
+        const { book } = this.props;
+        return (
+            <li>
+                <div className="book">
+                    <div className="book-top">
+                        <div className="book-cover" style={{
+                            width: 128,
+                            height: 193,
+                            backgroundImage: `url("${book.imageLinks.thumbnail}")`
+                        }}></div>
+                        <div className="book-shelf-changer">
+                            <select onChange={this.changeShelf} value={book.shelf}>
+                                <option value="none" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="book-title">{book.title}</div>
+                    <div className="book-authors">{book.authors}</div>
+                </div>
+            </li>
+        )
+    }
+}
+
+
+export default BookItem;
