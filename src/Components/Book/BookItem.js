13dfc74180f7d72614dd14f8fbaa9d692757a3b7
fabio.maiorano <fabio.maiorano@D-CB-001330>
2017-12-11 12:11:07 -0200

    update: material design on bookitem

--- src/Components/Book/BookItem.js
+++ src/Components/Book/BookItem.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+
 class BookItem extends Component {
 
-    changeShelf = (e) => {
-        this.props.updateBookShelf(e.target.value)
+    changeShelf = (event, value) => {
+        this.props.updateBookShelf(value);
     }
 
     render() {
@@ -18,13 +23,25 @@ class BookItem extends Component {
                             backgroundImage: `url("${book.imageLinks.thumbnail}")`
                         }}></div>
                         <div className="book-shelf-changer">
-                            <select onChange={this.changeShelf} value={book.shelf}>
+                        <IconMenu
+      iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+      anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+      targetOrigin={{horizontal: 'left', vertical: 'top'}}
+      onChange={this.changeShelf} value={book.shelf}
+    >
+      <MenuItem value="" primaryText="Move to..." disabled />
+      <MenuItem value="currentlyReading" primaryText="Currently Reading" onItemClick="currentlyReading" />
+      <MenuItem value="wantToRead" primaryText="Want to Read" />
+      <MenuItem value="read" primaryText="Read" />
+      <MenuItem value="none" primaryText="None" />
+    </IconMenu>
+                            {/* <select onChange={this.changeShelf} value={book.shelf}>
                                 <option value="none" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
                                 <option value="read">Read</option>
                                 <option value="none">None</option>
-                            </select>
+                            </select> */}
                         </div>
                     </div>
                     <div className="book-title">{book.title}</div>
